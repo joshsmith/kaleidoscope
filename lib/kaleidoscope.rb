bf9667c9f754cc91ca3f431f4e90dd6aa98c4ebb
Josh Smith <joshdotsmith@gmail.com>
2013-02-17 12:11:12 -0800

    Add outline of ClassMethods and InstanceMethods

--- lib/kaleidoscope.rb
+++ lib/kaleidoscope.rb
@@ -5,7 +5,16 @@ require 'kaleidoscope/railtie' if defined?(Rails)
 
 require 'kaleidoscope/configuration'
 require 'kaleidoscope/color'
+require 'kaleidoscope/instance_methods'
 
 module Kaleidoscope
   # Your code goes here...
+  module ClassMethods
+    def has_colors
+      include InstanceMethods
+
+      after_save :generate_colors
+      after_destroy :destroy_colors
+    end
+  end
 end
---
+++ lib/kaleidoscope/instance_methods.rb
@@ -0,0 +1,12 @@
+module Kaleidoscope
+  module InstanceMethods
+    def colors_for
+    end
+
+    def generate_colors
+    end
+
+    def destroy_colors
+    end
+  end
+end
