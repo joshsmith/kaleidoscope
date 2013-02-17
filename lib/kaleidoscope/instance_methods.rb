4d13c87963947f0ac6f068cfb0769da8a8baab75
Josh Smith <joshdotsmith@gmail.com>
2013-02-17 13:28:10 -0800

    Add log messages to instance methods

--- lib/kaleidoscope/instance_methods.rb
+++ lib/kaleidoscope/instance_methods.rb
@@ -4,9 +4,11 @@ module Kaleidoscope
     end
 
     def generate_colors
+      Kaleidoscope.log("Generating colors.")
     end
 
     def destroy_colors
+      Kaleidoscope.log("Deleting colors.")
     end
   end
 end
